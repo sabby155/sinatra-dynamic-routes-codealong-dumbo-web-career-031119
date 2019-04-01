cafdc37ad68c2857ba421d88360c121096369bd3
Sabrina Chowdhury <nebula155@gmail.com>
2019-04-01 20:54:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end  
   
-  get '/multiply/:num1/:num2' do 
+  post '/multiply/:num1/:num2' do 
     @num_1 = params[:num1]
     @num_2 = params[:num2]
     "#{@num1 * @num2}"
